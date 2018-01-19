3ac97260f7f39f3b1a84c65074f7bafb44519fda
epistrephein <epistrephein@gmail.com>
2018-01-19 09:14:23 +0100

    Output both stdout and stderr to logfile

--- config/schedule.rb
+++ config/schedule.rb
@@ -4,8 +4,8 @@ env :PATH, ENV['PATH']
 
 set :chronic_options, hours24: true
 set :output,
-    standard: File.join(Dir.pwd, 'log', 'orders.log'),
-    error:    File.join(Dir.pwd, 'log', 'errors.log')
+    standard: File.join(Dir.pwd, 'log', 'stdout.log'),
+    error:    File.join(Dir.pwd, 'log', 'stderr.log')
 
 every 1.day, at: '9:10' do
   ruby 'gdax_dca.rb'
